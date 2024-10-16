8a0b751d4fa7447667f18cc8bef23ffc8000d1ba
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-10-16 01:48:05 -0500

    merge both

---
+++ DTO/CreateShowDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace FullstackBeatsBE.DTO
+{
+    public class CreateShowDTO
+    {
+        public int HostId { get; set; }
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Rsvps { get; set; } = 0;
+        public DateTime AirDate { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
