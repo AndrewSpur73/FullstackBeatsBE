34d304169bc472b4cb2a658034fe7d07501c56ac
Andrew <andrewspurlock@rocketmail.com>
2024-10-15 19:53:05 -0500

    Single user details, update user, and delete user created and working

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -49,6 +49,38 @@ namespace FullstackBeatsBE.API
 
                 return Results.Ok(user);
             });
+
+            //Update User
+            app.MapPatch("/users/{id}", (FullstackBeatsBEDbContext db, int id, User user) =>
+            {
+                User userToUpdate = db.Users.SingleOrDefault(u => u.Id == id);
+                if (userToUpdate == null)
+                {
+                    return Results.NotFound();
+                }
+                userToUpdate.Uid = user.Uid;
+                userToUpdate.UserName = user.UserName;
+                userToUpdate.Image = user.Image;
+                userToUpdate.Email = user.Email;
+                userToUpdate.About = user.About;
+                db.SaveChanges();
+                return Results.Ok(userToUpdate);
+            });
+
+            // Delete User
+            app.MapDelete("/users/{id}", (FullstackBeatsBEDbContext db, int id) =>
+            {
+                var userToDelete = db.Users.FirstOrDefault(s => s.Id == id);
+
+                if (userToDelete == null)
+                {
+                    return Results.NotFound("No user with matching id");
+                }
+
+                db.Users.Remove(userToDelete);
+                db.SaveChanges();
+                return Results.Ok("User deleted");
+            });
         }
     }
 }
